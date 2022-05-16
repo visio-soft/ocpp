cd7ca63d2a230c7e9a58ea82977e75b2a0ba8b03
simonask <simonask42@gmail.com>
2022-05-16 10:15:51 +0200

    Tables for CP, meter, status. v201 updated

--- cpo_main/database/models.py
+++ cpo_main/database/models.py
@@ -8,8 +8,20 @@ class User(Base):
     username = Column(String)
     password = Column(String)
 
-class ChargePoint(Base):
-    __tablename__ = "charge_point"
+class Token(Base):
+    __tablename__ = "token"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    password = Column(String)
+    token = Column(String)
+
+
+"""
+OCPP version 1.6 models
+"""
+
+class ChargePoint16(Base):
+    __tablename__ = "charge_point_16"
     id = Column(Integer, primary_key=True, index=True)
     charge_point_id = Column(String)
     charge_point_vendor = Column(String)
@@ -23,25 +35,20 @@ class ChargePoint(Base):
     meter_type = Column(String)
     heartbeat = Column(DateTime)
 
-class Token(Base):
-    __tablename__ = "token"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
-    password = Column(String)
-    token = Column(String)
 
-class ChargePointSessions(Base):
-    __tablename__ = "charging_session"
-    id = Column(Integer, primary_key=True, index=True)
-    transaction_id = Column(Integer)
+
+class ChargePointSessions16(Base):
+    __tablename__ = "charging_session_16"
+    transaction_id = Column(Integer, primary_key=True, index=True)
     charge_point_id= Column(String)
+    connector_id = Column(Integer)
     id_tag= Column(String)
     meter_start = Column(Integer)
     timestamp = Column(DateTime)
     reservation_id = Column(String)
 
-class PastChargePointSessions(Base):
-    __tablename__ = "past_sessions"
+class PastChargePointSessions16(Base):
+    __tablename__ = "past_sessions_16"
     id = Column(Integer, primary_key=True, index=True)
     transaction_id = Column(Integer)
     charge_point_id = Column(String)
@@ -51,11 +58,10 @@ class PastChargePointSessions(Base):
     reason = Column(String)
 
 
-class ChargePointStatus(Base):
-    __tablename__ = "charge_point_status"
+class ChargePointStatus16(Base):
+    __tablename__ = "charge_point_status_16"
     charge_point_id = Column(String, primary_key=True, index=True)
     connector_id = Column(Integer)
-    timestamp = Column(DateTime)
     status = Column(String)
     error_code = Column(String)
     status_timestamp = Column(DateTime)
@@ -64,8 +70,8 @@ class ChargePointStatus(Base):
     vendor_error_code = Column(String)
     
 
-class MeterValues(Base):
-    __tablename__ = "meter_values"
+class MeterValues16(Base):
+    __tablename__ = "meter_values_16"
     id = Column(Integer, primary_key=True, index=True)
     transaction_id = Column(Integer)
     charge_point_id= Column(String)
@@ -79,8 +85,96 @@ class MeterValues(Base):
     location = Column(String)
     unit = Column(String)
 
-class Schedule(Base):
-    __tablename__ = "schedule"
+class Schedule16(Base):
+    __tablename__ = "schedule_16"
+    id = Column(Integer, primary_key=True, index=True)
+    charge_point_id= Column(String)
+    name= Column(String)
+    active= Column(Boolean)
+    start_hours= Column(Integer)
+    start_minutes= Column(Integer)
+    end_hours= Column(Integer)
+    end_minutes= Column(Integer)
+    time_zone= Column(String)
+    monday= Column(Boolean)
+    tuesday= Column(Boolean)
+    wednesday= Column(Boolean)
+    thursday= Column(Boolean)
+    friday= Column(Boolean)
+    saturday= Column(Boolean)
+    sunday= Column(Boolean)
+    connector_id_list= Column(String)
+
+
+"""
+OCPP version 2.0.1 models
+"""
+
+class ChargePoint201(Base):
+    __tablename__ = "charge_point_201"
+    id = Column(Integer, primary_key=True, index=True)
+    charge_point_id = Column(String)
+    charge_point_vendor = Column(String)
+    charge_point_model = Column(String)
+    charge_point_serial_number = Column(String)
+    firmware_version = Column(String)
+    charge_box_serial_number = Column(String)
+    iccid = Column(String)
+    imsi = Column(String)
+    meter_serial_number = Column(String)
+    meter_type = Column(String)
+    heartbeat = Column(DateTime)
+
+class ChargePointSessions201(Base):
+    __tablename__ = "charging_session_201"
+    transaction_id = Column(Integer, primary_key=True, index=True)
+    charge_point_id= Column(String)
+    connector_id = Column(Integer)
+    id_tag= Column(String)
+    meter_start = Column(Integer)
+    timestamp = Column(DateTime)
+    reservation_id = Column(String)
+
+class PastChargePointSessions201(Base):
+    __tablename__ = "past_sessions_201"
+    id = Column(Integer, primary_key=True, index=True)
+    transaction_id = Column(Integer)
+    charge_point_id = Column(String)
+    id_tag = Column(String)
+    meter_stop = Column(Integer)
+    timestamp = Column(DateTime)
+    reason = Column(String)
+
+
+class ChargePointStatus201(Base):
+    __tablename__ = "charge_point_status_201"
+    charge_point_id = Column(String, primary_key=True, index=True)
+    connector_id = Column(Integer)
+    status = Column(String)
+    error_code = Column(String)
+    status_timestamp = Column(DateTime)
+    info = Column(String)
+    vendor_id = Column(String)
+    vendor_error_code = Column(String)
+    
+
+class MeterValues201(Base):
+    __tablename__ = "meter_values_201"
+    id = Column(Integer, primary_key=True, index=True)
+    transaction_id = Column(Integer)
+    charge_point_id= Column(String)
+    connector_id= Column(Integer)
+    timestamp = Column(DateTime)
+    measurand = Column(String) 
+    value = Column(String)
+    context = Column(String)
+    format = Column(String)
+    phase = Column(String)
+    location = Column(String)
+    unit = Column(String)
+
+class Schedule201(Base):
+    __tablename__ = "schedule_201"
     id = Column(Integer, primary_key=True, index=True)
     charge_point_id= Column(String)
     name= Column(String)
