2715e6fc98a05dbb5b851877b52e0888cf93540d
simonask <simonask42@gmail.com>
2022-06-01 15:13:02 +0200

    Clean v201 repos

--- src/v201/CPO/router/authentication.py
+++ src/v201/CPO/router/authentication.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends, status, HTTPException
-from v201.CPO import token
+from v201.CPO.Auth import token
 from resources import models
 from resources.database import get_db
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from v201.CPO.hashing import Hash
+from v201.CPO.Hashing.hashing import Hash
 
 
 router = APIRouter(tags=["Authentication"])
